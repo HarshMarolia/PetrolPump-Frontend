01d146a9aade3be36497ee329698f55ea51fd501
nikhimishra03 <nikhilm0311@gmail.com>
2025-10-05 14:09:13 +0530

    constant link updated

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,8 @@
-const SERVER_URL = "https://petrolpump-backend.onrender.com/v1/api";
+
+const SERVER_URL = "https://api.fuelinfo.in/v1/api";
+
 /*
+const SERVER_URL = "https://petrolpump-backend.onrender.com/v1/api";
 https://petrolpump-backend.onrender.com
 http://localhost:8000
 */
